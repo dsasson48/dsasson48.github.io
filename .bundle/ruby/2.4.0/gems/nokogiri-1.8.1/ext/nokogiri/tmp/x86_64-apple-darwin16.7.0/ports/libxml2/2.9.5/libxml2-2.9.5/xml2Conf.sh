0f781c2ef98266939a65591645120248a1f000fd
David Sasson <dsasson@hpsh.harvard.edu>
2017-11-25 15:00:47 -0500

    index changes

---
+++ .bundle/ruby/2.4.0/gems/nokogiri-1.8.1/ext/nokogiri/tmp/x86_64-apple-darwin16.7.0/ports/libxml2/2.9.5/libxml2-2.9.5/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/DavidSasson/Desktop/My Websites/Personal/dsasson48.github.io/.bundle/ruby/2.4.0/gems/nokogiri-1.8.1/ports/x86_64-apple-darwin16.7.0/libxml2/2.9.5/lib"
+XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/DavidSasson/Desktop/My Websites/Personal/dsasson48.github.io/.bundle/ruby/2.4.0/gems/nokogiri-1.8.1/ports/x86_64-apple-darwin16.7.0/libxml2/2.9.5/include/libxml2"
+MODULE_VERSION="xml2-2.9.5"
+
